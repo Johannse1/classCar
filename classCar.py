99c4821557091308dd7487dfdb698f0de79a4731
Evan <johannse@hartwick.edu>
2019-11-11 21:21:35 -0500

    update

---
+++ classCar.py
@@ -0,0 +1,16 @@
+# Evan Johanns
+# class Car
+# 11/11/19
+
+
+class Car(object):
+
+    def __init__(self,make,model,mileage,price,color):
+        self.make = make
+        self.model = model
+        self.mileage = mileage
+        self.price = price
+        self.color = color
+    def description(self):
+
+
